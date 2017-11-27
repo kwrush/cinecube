f642376aad137f093684b4a8c9eef80a5bf51662
Kai Wang <k.wang0905@hotmail.com>
2017-11-27 23:27:24 +0100

    add backend api

--- server/routes/movie/index.js
+++ server/routes/movie/index.js
@@ -5,12 +5,47 @@
 'use strict';
 
 var router = require('express').Router();
+var camelCaseKey = require('../../middlewares/camelCaseKey');
 
 router.get('/popular', (req, res) => {
   var tmdb = req.app.locals.tmdb;
   tmdb
     .miscPopularMovies((err, tmdbRes) => {
       if (err) res.send(err);
+
+      tmdbRes = camelCaseKey(tmdbRes);
+
+      res.json(tmdbRes);
+    });
+});
+
+router.get('/in_theatre', (req, res) => {
+  var tmbd = req.app.locals.tmbd;
+  tmdb
+    .miscNowPlayingMovies((err, tmdbRes) => {
+      if (err) res.send(err);
+      tmdbRes = camelCaseKey(tmdbRes);
+      res.json(tmdbRes);
+    });
+});
+
+router.get('/top_rated', (req, res) => {
+  var tmdb = req.app.locals.tmbd;
+  tmdb
+    .miscTopRatedMovies((err, tmdbRes) => {
+      if (err) res.send(err);
+      tmdbRes = camelCaseKey(tmdbRes);
+
+      res.json(tmdbRes);
+    });
+});
+
+router.get('/upcoming', (req, res) => {
+  var tmdb = req.app.locals.tmbd;
+  tmdb
+    .miscUpcomingMovies((err, tmdbRes) => {
+      if (err) res.send(err);
+      tmdbRes = camelCaseKey(tmdbRes);
 
       res.json(tmdbRes);
     });
@@ -22,8 +57,14 @@ router.get('/:id', (req, res) => {
     .movieInfo({ id: req.params.id }, (err, tmdbRes) => {
       if (err) res.send(err);
 
+      tmdbRes = camelCaseKey(tmdbRes);
+
       res.json(tmdbRes);
     });
+});
+
+router.get('/search', (req, res) => {
+  
 });
 
 module.exports = router;
--- server/routes/tv/index.js
+++ server/routes/tv/index.js
@@ -5,12 +5,38 @@
 'use strict';
 
 var router = require('express').Router();
+var camelCaseKey = require('../../middlewares/camelCaseKey');
 
 router.get('/popular', (req, res) => {
   var tmdb = req.app.locals.tmdb;
   tmdb
     .miscPopularTvs((err, tmdbRes) => {
       if (err) res.send(err);
+
+      tmdbRes = camelCaseKey(tmdbRes);
+
+
+      res.json(tmdbRes);
+    });
+});
+
+router.get('/top_rated', (req, res) => {
+  var tmdb = req.app.locals.tmbd;
+  tmdb
+    .miscTopRatedTvs((err, tmdbRes) => {
+      if (err) res.send(err);
+      tmdbRes = camelCaseKey(tmdbRes);
+
+      res.json(tmdbRes);
+    });
+});
+
+router.get('/airing', (req, res) => {
+  var tmdb = req.app.locals.tmbd;
+  tmdb
+    .tvAiringToday((err, tmdbRes) => {
+      if (err) res.send(err);
+      tmdbRes = camelCaseKey(tmdbRes);
 
       res.json(tmdbRes);
     });
@@ -19,11 +45,17 @@ router.get('/popular', (req, res) => {
 router.get('/:id', (req, res) => {
   var tmdb = req.app.locals.tmdb;
   tmdb
-    .tvInfo({ id: req.params.id }, (err, tmdbRes) => {
+    .movieInfo({ id: req.params.id }, (err, tmdbRes) => {
       if (err) res.send(err);
+
+      tmdbRes = camelCaseKey(tmdbRes);
 
       res.json(tmdbRes);
     });
+});
+
+router.get('/search', (req, res) => {
+  
 });
 
 module.exports = router;
