0b65ab719dbadd7934fdc6566c0dc5580e583b01
Kai Wang <k.wang0905@hotmail.com>
2018-07-13 23:58:20 +0200

    init, remove old codes and plan to rewrite it from scratch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
-import App from 'containers/App/index';
-
+import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render((
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>
-), document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
