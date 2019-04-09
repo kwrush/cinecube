fbcbe1107ec5b52149a1e9e598655e5a7c840548
kwrush <kaiwang.me@gmail.com>
2019-04-10 01:04:36 +0200

    refine styles

--- src/client/components/Header/Header.test.js
+++ src/client/components/Header/Header.test.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
-import { Header } from '../Header';
+import { Header, Toggler } from '../Header';
+import { Sidebar } from '../Sidebar';
+import { IoMdClose } from 'react-icons/io';
+import HeaderSearch from './HeaderSearch';
 
 describe('<Header /> test', () => {
-  it('should hide search input initially', () => {
-    const wrapper = shallow(<Header />);
-    expect(wrapper.find('header-search-input.show')).toHaveLength(0);
+  it('should toggle sidebar', () => {
+    const wrapper = mount(<MemoryRouter><Header /></MemoryRouter>);
+    expect(wrapper.find(Sidebar).props().open).toBeFalsy();
+    wrapper.find(Toggler).first().simulate('click');
+    expect(wrapper.update().find(Sidebar).props().open).toBeTruthy();
   });
 
-  it('should show search input when click the search button', () => {
-    const wrapper = mount(<MemoryRouter><Header /></MemoryRouter>);
-    wrapper.find('button.search-toggler').first().simulate('click');
-    expect(wrapper.update().find('.header-search-input.show')).toHaveLength(1);
+  it('should toggle the search', () => {
+    const wrapper = mount(<MemoryRouter><HeaderSearch /></MemoryRouter>);
+    expect(wrapper.find(IoMdClose)).toHaveLength(0);
+    wrapper.find(Toggler).simulate('click');
+    jest.useFakeTimers();
+    jest.runTimersToTime(300);
+    expect(wrapper.update().find(IoMdClose)).toHaveLength(1);
   });
 });
--- src/client/components/Header/HeaderSearch.js
+++ src/client/components/Header/HeaderSearch.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
-import { Button } from 'reactstrap';
 import { 
   IoMdSearch,
   IoMdClose
